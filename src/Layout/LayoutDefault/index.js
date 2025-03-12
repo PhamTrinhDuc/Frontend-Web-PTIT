7e9f1ed72a19a9bdda673c4e7deefbd3a892ab68
PhamTrinhDuc <duc78240@gmail.com>
2025-03-13 00:42:05 +0700

    add layout website

--- src/Layout/LayoutDefault/index.js
+++ src/Layout/LayoutDefault/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavLink, Link, Outlet } from 'react-router-dom';
 import Header from '../Header';
 import Footer from '../Footer';
+import './LayoutDefault.scss';
+
 
 function LayoutDefault() {
 
