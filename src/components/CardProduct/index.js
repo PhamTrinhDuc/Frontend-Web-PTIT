1c68a8b9cfb14db406072e1cf874dc30e16fe68d
PhamTrinhDuc <duc78240@gmail.com>
2025-03-12 09:33:45 +0700

    add card product to components

---
+++ src/components/CardProduct/index.js
@@ -0,0 +1,46 @@
+import {Row, Col, Card, Badge, Rate} from 'antd';
+import { Typography } from 'antd';
+import { HeartOutlined, EyeOutlined } from '@ant-design/icons';
+import './Card.scss';
+
+
+const { Text } = Typography;
+
+
+function CardProduct(props) {
+  const { product } = props;
+  return (
+    <>
+      <div className="card-container">
+        <Card 
+          className="card-image"
+          cover={
+            <div className="product-image-container">
+              <Badge.Ribbon text="Sale" color="#DB4444" className="sale-badge">
+                <img src={product.image} alt={product.name} className="product-image" />
+              </Badge.Ribbon>
+            </div>
+          }
+          actions={[
+            <HeartOutlined key="heart" className="product-action-icon" />,
+            <EyeOutlined key="view" className="product-action-icon" />
+          ]}
+        >
+        </Card>
+        <div className='card-content'>
+          <h3 className='product-title'>{product.name}</h3>
+          <div className='product-price'>
+            <Text className='original-price'>{product.originalPrice}</Text>
+            <Text className='sale-price'>{product.salePrice }</Text>
+          </div>
+          <div className='product-rating'>
+            <Rate disabled defaultValue={product.rating} />
+            <Text className='review-count'>({product.reviews})</Text>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CardProduct;
