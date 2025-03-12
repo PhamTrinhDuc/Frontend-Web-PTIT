be47ca4c68e2f797c0d3539ffd1027c84f2ed071
PhamTrinhDuc <duc78240@gmail.com>
2025-03-13 00:41:02 +0700

    add component service

---
+++ src/components/Service/index.js
@@ -0,0 +1,42 @@
+import {Row, Col} from 'antd';
+import { FaShippingFast } from "react-icons/fa";
+import { RiCustomerServiceFill } from "react-icons/ri";
+import { SiAdguard } from "react-icons/si";
+import './Service.scss';
+
+function Service () {
+  return (
+    <>
+      <div className="service-container">
+        <Row gutter={[16, 16]}>
+          <Col xs={24} md={12} lg={8}>
+            <div className="service-card">
+              <FaShippingFast className="service-icon"/>
+              <h2>FREE AND FAST DELIVERY</h2>
+              <p>Free delivery for all orders over $140</p>
+            </div>
+          </Col>
+
+          <Col xs={24} md={12} lg={8}>
+            <div className="service-card">
+              <RiCustomerServiceFill className="service-icon"/>
+              <h2>24/7 CUSTOMER SERVICE</h2>
+              <p>Friendly 24/7 customer support</p>
+            </div>
+          </Col>
+
+          <Col xs={24} md={12} lg={8}>
+            <div className="service-card">
+              <SiAdguard className="service-icon"/>
+              <h2>MONEY BACK GUARANTEE</h2>
+              <p>We reurn money within 30 days</p>
+            </div>
+          </Col>
+
+        </Row>
+      </div>
+    </>
+  )
+}
+
+export default Service;
