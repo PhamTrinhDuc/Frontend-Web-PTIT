1bd09f56b59d2d476f7d758abb8557d79987a25b
PhamTrinhDuc <duc78240@gmail.com>
2025-03-12 13:16:59 +0700

    update page error

--- src/pages/user/Error/index.js
+++ src/pages/user/Error/index.js
@@ -1,8 +1,13 @@
+
+import { Button } from 'antd';
+import './Error.scss'
+
 function Error() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
+    <div className='error-page'>
+      <h1 className='code'>404 Not Found</h1>
+      <p className='massage'>Your visited page not found. You may go home page.</p>
+      <Button className='back-home' type="ghost" href="/">Back to home page</Button>
     </div>
   );
 }
